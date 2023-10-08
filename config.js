6e4f7559de8c2c815bf63444d4a2b2d6e7f2607b
Fr3zyy <123799898+Fr3zyy@users.noreply.github.com>
2023-10-08 19:00:00 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
     bot: {
-        token: "MTE1NTk2MjMxNjc5ODU3NDYzMg.Gkgz1o.R_Ywz2wYkba497zAUkONlQxWvdCBx1uIn0fpHs", //botun tokeni
+        token: "", //botun tokeni
         prefix: ".", //botun prefixi
-        mongoDb: "mongodb+srv://fr3zycode123:fr3zycode123@cluster0.qrhvvwa.mongodb.net/?retryWrites=true&w=majority" //MongoDB Url. Youtubede araştırarak nasıl alınacagını öürenebilirsiniz
+        mongoDb: "" //MongoDB Url. Youtubede araştırarak nasıl alınacagını öürenebilirsiniz
     },
     coinflip: {
         coinFlipMax: 250000, //Coinflip en fazla oynayabilecekleri para
